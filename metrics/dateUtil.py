6bc0d4b0db1f05dc497c93dcbb6c7ca82a904b1a
beng-git <benjamin.nguyen@insa-cvl.fr>
2022-06-03 19:55:07 +0200

    Rajout des métriques

---
+++ metrics/dateUtil.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+def main(df_anon, df_original, params, nb_orig_lines):
+    df_orig = df_original.copy()
+    df_anon = df_anon.copy()
+
+    df_date_utility = pd.DataFrame({ 'DayOfTheWeek_orig': df_orig['DateTime'].dt.dayofweek, 'DayOfTheWeek_anon': df_anon['DateTime'].dt.dayofweek, 'Week_orig':df_orig['DateTime'].dt.isocalendar().week, 'Week_anon': df_anon['DateTime'].dt.isocalendar().week })
+    df_date_utility['DiffDate'] = abs(df_date_utility['DayOfTheWeek_orig']-df_date_utility['DayOfTheWeek_anon'])
+    #pour tout changement de semaine l'utilite doit etre 0 
+    df_date_utility.loc[~(df_date_utility['Week_orig']==df_date_utility['Week_anon']),'DiffDate']=7
+    df_date_utility['date_util']= 1- df_date_utility['DiffDate']/7
+
+    score = df_date_utility["date_util"].mean().round(3)
+    return score
---
+++ metrics/utility_distance.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+# cette metrique calcule la difference de distance entre le point GPS original et les nouvelles lat lon (en km) en utilisant la metrique de Haversine
+#le score final est l'inverse de la moyenne de diffrence de distance calculee pour chauqe point
+def main(df_anon, df_original, params, nb_orig_lines):
+
+    df_orig = df_original.copy()
+    df_anon = df_anon.copy()
+    df_orig.rename(columns={'id':'ID', 'date':'DateTime', 'latitude':'lat', 'longitude':'lon'},inplace=True)
+    df_anon.rename(columns={'id':'ID', 'date':'DateTime', 'latitude':'lat', 'longitude':'lon'},inplace=True)
+    df_anon.rename(columns={'ID':'ID_ano', 'DateTime':'DateTime_ano', 'lat':'lat_ano', 'lon':'lon_ano'}, inplace = True)
+    
+    df_orig['lat']= df_orig['lat'].astype('float64')
+    df_orig['lon']= df_orig['lon'].astype('float64')
+    df_anon['lat_ano']= df_anon['lat_ano'].astype('float64')
+    df_anon['lon_ano']= df_anon['lon_ano'].astype('float64')
+    
+    df = pd.concat([df_orig.reset_index(drop=True),df_anon.reset_index(drop=True)], axis=1)
+   
+    #Haversine metrqiue
+    to_radians = np.pi/180
+    R = 6371.009 #en km
+    #a=np.sin(((df.lat*to_radians-df.lat_ano*to_radians)/2)**2) + np.sin((((df.lon*to_radians-df.lon_ano*to_radians)/2)**2))*np.cos(df.lat*to_radians)*np.cos(df.lat_ano*to_radians)
+    a = np.sin(((df.lat*to_radians-df.lat_ano*to_radians)/2))**2 + (np.sin((((df.lon*to_radians-df.lon_ano*to_radians)/2)))**(2))*np.cos(df.lat*to_radians)*np.cos(df.lat_ano*to_radians)
+    b = np.sqrt(a)
+    df['Haversine_score']= 2 * R * np.arcsin(b)
+    if(df["Haversine_score"].mean().round(3)==0):
+        score = 1
+    else:
+        #print(df["Haversine_score"].mean())
+        score = (1/(df["Haversine_score"].mean())).round(3)
+    return score
